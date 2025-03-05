57fc55ef6a2c871ae5fcc740f370cd3136cea87a
Nur_Alislam_Kastiro <22331549@studentmail.ul.ie>
2025-03-05 19:11:05 +0000

    Add fragments for Home, Market, Profile, and Timetable sections and update related layouts

--- app/src/main/java/com/example/appdevelopmentprojectfinal/MainActivity.java
+++ app/src/main/java/com/example/appdevelopmentprojectfinal/MainActivity.java
@@ -7,18 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.appdevelopmentprojectfinal.databinding.ActivityMainBinding;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        We will set here the listener to send from the bottom navigation bar items (icons) to the appropriate fragment.
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.market:
+                    replaceFragment(new MarketFragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.timetable:
+                    replaceFragment(new TimetableFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
         });
+    }
+//    the process of changing of the fragment described here
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
